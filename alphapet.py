ab0af42df7f30c4e4d6d76aafb878116daf6f1f4
Logeshvarshith <36299174+Logeshvarshith@users.noreply.github.com>
2018-02-15 14:08:09 +0530

    Update alphapet.py

--- alphapet.py
+++ alphapet.py
@@ -1,9 +1,5 @@
 ch=input(("Enter the string:"))
-
 if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-			
-	print("It is an alphabet")
-
+  print("It is an alphabet")
 else:
-			
-	print("It is not an alphabet")
+  print("It is not an alphabet")
