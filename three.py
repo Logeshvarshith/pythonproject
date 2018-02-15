23a1bad27b7b3d6f4665a2b82570c4baaf2edaae
Logeshvarshith <36299174+Logeshvarshith@users.noreply.github.com>
2018-02-15 14:17:14 +0530

    Update three.py

--- three.py
+++ three.py
@@ -1,17 +1,9 @@
 a=int(input("Enter the number:"))
-
 b=int(input("Enter the number:"))
-
 c=int(input("Enter the number:"))
-
 if(a>b and a>c):
-  
-	print("a is greater")
-
+  print("a is greater")
 elif(b>a and b>c):
-  
-	print("b is greater")
-
+  print("b is greater")
 else:
-  
-	print("c is greater")
+  print("c is greater")
