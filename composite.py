380d2b573c3397b603c2c31f903e47894ae133ae
Logeshvarshith <36299174+Logeshvarshith@users.noreply.github.com>
2018-03-14 12:28:22 +0530

    Update composite.py

--- composite.py
+++ composite.py
@@ -1,11 +1,9 @@
 n=int(input("Enter the number:"))
-a=0
-for i in range(2,n):
-    n%i==0
-    a=i
 if n>1:
-  print ('Yes')
-elif n==1:
+    for i in range(2,n):
+        if n%i==0:
+            print ('Yes')
+    else:
+        print("no")
+elif n==1 or n==0:
   print ('neither prime nor composite')
-else:
-  print("n0")
