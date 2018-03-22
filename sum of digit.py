e61fed2ec2d18a955df8d1c278097f940ac12ad1
Logeshvarshith <36299174+Logeshvarshith@users.noreply.github.com>
2018-03-22 15:03:47 +0530

    Update sum of digit.py

--- sum of digit.py
+++ sum of digit.py
@@ -1,6 +1,6 @@
-n=int(input("Enter the terms:"))
-a=[]
-for i in range(1,n+1):
-  b=int(input("Enter the numbers:"))
-  a.append(b)
-print(sum(a))
+m=int(input("Enter the terms:"))
+n=[]
+for i in range(1,m+1):
+  c=int(input("Enter the numbers:"))
+  n.append(c)
+print(sum(n))
